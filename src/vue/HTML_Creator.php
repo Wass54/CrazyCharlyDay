96e22489b99c2d48429719752925b6a6c1f23b61
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-03-10 15:35:15 +0100

    Update HTML_Creator.php

--- src/vue/HTML_Creator.php
+++ src/vue/HTML_Creator.php
@@ -18,6 +18,7 @@ class HTML_Creator{
     public function html_body(string $content): string{
 
         $url_produit = $this->container->router->pathFor('produit');
+        $url_categorie = $this->container->router->pathFor('categorie');
         $url_accueil = $this->container->router->pathFor('accueil');
         $url_js = $this->container->router->pathFor('accueil') . "js";
         $url_css = $this->container->router->pathFor('accueil') . "css";
@@ -46,24 +47,25 @@ class HTML_Creator{
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="$url_produit">Produit</a></li>
+                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="$url_categorie">Catégories</a></li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Boxes</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                    <li><a class="dropdown-item" href="#!">Liste des boxes</a></li>
                                     <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="$url_produit">Produit</a></li>
-                                    <li><a class="dropdown-item" href="#!">Categorie</a></li>
+                                    <li><a class="dropdown-item" href="#!">Box1</a></li>
+                                    <li><a class="dropdown-item" href="#!">Box2</a></li>
                                 </ul>
                             </li>
                         </ul>
-                        <form class="d-flex">
-                            <button class="btn btn-outline-dark" type="submit">
-                                <i class="bi-cart-fill me-1"></i>
-                                Cart
-                                <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                            </button>
-                        </form>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-gift-fill me-1"></i>
+                            Boxes
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
                     </div>
                 </div>
             </nav>
